d06107cf6215d9c1677c7ed191d54ecadf22fc4b
Gideon <llama@ordorbles.uk>
2018-04-15 14:07:35 +0100

    start of layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var sassGlobal = require('gulp-sass-glob');
+var sassGlob = require('gulp-sass-glob');
+var concat = require('gulp-concat');
 
 gulp.task('styles', function() {
     return gulp.src('styles/main.scss')
         .pipe(sassGlob())
-        .pipe(sass())
-        .pipe(gulp.dest('style.css'));
+        .pipe(sass()).pipe(concat('style.css'))
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', ['styles']);
